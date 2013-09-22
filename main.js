8eccb49fbebd297a2151b2e0149c3ca4f646c0f5
John-David Dalton <john.david.dalton@gmail.com>
2013-09-22 14:46:09 -0700

    Bump to v2.1.0.

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
 /**
  * Lo-Dash 2.1.0 (Custom Build) <http://lodash.com/>
- * Build: `lodash modularize exports="amd" -o ./compat/`
+ * Build: `lodash underscore modularize exports="amd" -o ./underscore/`
  * Copyright 2012-2013 The Dojo Foundation <http://dojofoundation.org/>
  * Based on Underscore.js 1.5.2 <http://underscorejs.org/LICENSE>
  * Copyright 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
  * Available under MIT license <http://lodash.com/license>
  */
-define([], function() {
-
-  /** Used as the max size of the `arrayPool` and `objectPool` */
-  var maxPoolSize = 40;
-
-  return maxPoolSize;
+define(['./modern/main'], function(lodash) {
+  return lodash;
 });
