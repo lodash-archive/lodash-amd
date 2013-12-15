755d315ed885a7e7b4a3a59d415cebfbfebbc2a1
John-David Dalton <john.david.dalton@gmail.com>
2013-12-14 20:38:58 -0800

    Remove `support` dep from the Underscore build of `isElement`.

--- underscore/objects/isElement.js
+++ underscore/objects/isElement.js
@@ -6,7 +6,7 @@
  * Copyright 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
  * Available under MIT license <http://lodash.com/license>
  */
-define(['../support'], function(support) {
+define([], function() {
 
   /**
    * Checks if `value` is a DOM element.
