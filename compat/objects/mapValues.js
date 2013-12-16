85e8ee8b9cd330021b327a01786a2ab3a048c8bf
John-David Dalton <john.david.dalton@gmail.com>
2013-12-16 09:14:32 -0800

    Fix docs in `mapValues` and `sample`. [ci skip]

--- compat/objects/mapValues.js
+++ compat/objects/mapValues.js
@@ -29,7 +29,7 @@ define(['../functions/createCallback', './forOwn'], function(createCallback, for
    *  per iteration. If a property name or object is provided it will be used
    *  to create a "_.pluck" or "_.where" style callback, respectively.
    * @param {*} [thisArg] The `this` binding of `callback`.
-   * @returns {Array} Returns a new object with values of the results of each `callback` execution.
+   * @returns {Object} Returns a new object with values of the results of each `callback` execution.
    * @example
    *
    * _.mapValues({ 'a': 1, 'b': 2, 'c': 3} , function(num) { return num * 3; });
