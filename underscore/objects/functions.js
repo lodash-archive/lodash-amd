ef523cbeffb5756493d8f187ccd5b0dc35d770bb
John-David Dalton <john.david.dalton@gmail.com>
2014-03-09 13:24:56 -0700

    Doc tweaks in `functions`. [ci skip]

--- underscore/objects/functions.js
+++ underscore/objects/functions.js
@@ -17,7 +17,7 @@ define(['../internals/baseForIn', './isFunction'], function(baseForIn, isFunctio
    * @alias methods
    * @category Objects
    * @param {Object} object The object to inspect.
-   * @returns {Array} Returns an array of property names that have function values.
+   * @returns {Array} Returns the new sorted array of property names.
    * @example
    *
    * _.functions(_);
@@ -25,6 +25,7 @@ define(['../internals/baseForIn', './isFunction'], function(baseForIn, isFunctio
    */
   function functions(object) {
     var result = [];
+
     baseForIn(object, function(value, key) {
       if (isFunction(value)) {
         result.push(key);
