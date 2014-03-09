ea86631560de612704e4818755446b8fa236ae57
John-David Dalton <john.david.dalton@gmail.com>
2014-03-09 13:05:30 -0700

    Add `drop`, `dropRight`, `dropRightWhile`, `dropWhile`, `take`, `takeRight`, `takeRightWhile`, `takeWhile`, `negate`, `baseValues`, `valuesIn`, `baseFor`, `baseForRight`, `keysIn`, `kebabCase`, `pad`, `padLeft`, `padRight`, `repeat`, `snakeCase`, `startsWith`, `escapeRegExp`, `createCompounder`, `createPad`, `deburrLetter`, and `truncate`.

---
+++ underscore/strings.js
@@ -0,0 +1,18 @@
+/**
+ * Lo-Dash 2.4.1 (Custom Build) <http://lodash.com/>
+ * Build: `lodash modularize underscore exports="amd" -o ./underscore/`
+ * Copyright 2012-2014 The Dojo Foundation <http://dojofoundation.org/>
+ * Based on Underscore.js 1.6.0 <http://underscorejs.org/LICENSE>
+ * Copyright 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+ * Available under MIT license <http://lodash.com/license>
+ */
+define(['./utilities/escape', './strings/escapeRegExp', './utilities/template', './utilities/templateSettings', './utilities/unescape'], function(escape, escapeRegExp, template, templateSettings, unescape) {
+
+  return {
+    'escape': escape,
+    'escapeRegExp': escapeRegExp,
+    'template': template,
+    'templateSettings': templateSettings,
+    'unescape': unescape
+  };
+});
