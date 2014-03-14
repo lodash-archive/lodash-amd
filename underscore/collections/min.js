359cfbd58839dffd2dd6c3072e9de7ecb73d3f4b
John-David Dalton <john.david.dalton@gmail.com>
2014-03-14 02:03:56 -0700

    Add `toLength` as a dep of the Underscore build of `max` and `min`.

--- underscore/collections/min.js
+++ underscore/collections/min.js
@@ -6,7 +6,7 @@
  * Copyright 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
  * Available under MIT license <http://lodash.com/license>
  */
-define(['../internals/baseEach', '../functions/createCallback'], function(baseEach, createCallback) {
+define(['../internals/baseEach', '../functions/createCallback', '../internals/toLength'], function(baseEach, createCallback, toLength) {
 
   /**
    * Retrieves the minimum value of a collection. If the collection is empty or
