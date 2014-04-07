3c5b83b9cc739798ce6ba2a4ad430be9f7a0d7a2
John-David Dalton <john.david.dalton@gmail.com>
2014-04-07 00:31:26 -0700

    Add methods to their flux categories.

---
+++ underscore/strings/templateSettings.js
@@ -0,0 +1,73 @@
+/**
+ * Lo-Dash 2.4.1 (Custom Build) <http://lodash.com/>
+ * Build: `lodash modularize underscore exports="amd" -o ./underscore/`
+ * Copyright 2012-2014 The Dojo Foundation <http://dojofoundation.org/>
+ * Based on Underscore.js 1.6.0 <http://underscorejs.org/LICENSE>
+ * Copyright 2009-2014 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+ * Available under MIT license <http://lodash.com/license>
+ */
+define(['../utilities/escape', '../internals/reEscape', '../internals/reEvaluate', '../internals/reInterpolate'], function(escape, reEscape, reEvaluate, reInterpolate) {
+
+  /**
+   * By default, the template delimiters used by Lo-Dash are similar to those
+   * in embedded Ruby (ERB). Change the following template settings to use
+   * alternative delimiters.
+   *
+   * @static
+   * @memberOf _
+   * @type Object
+   */
+  var templateSettings = {
+
+    /**
+     * Used to detect `data` property values to be HTML-escaped.
+     *
+     * @memberOf _.templateSettings
+     * @type RegExp
+     */
+    'escape': reEscape,
+
+    /**
+     * Used to detect code to be evaluated.
+     *
+     * @memberOf _.templateSettings
+     * @type RegExp
+     */
+    'evaluate': reEvaluate,
+
+    /**
+     * Used to detect `data` property values to inject.
+     *
+     * @memberOf _.templateSettings
+     * @type RegExp
+     */
+    'interpolate': reInterpolate,
+
+    /**
+     * Used to reference the data object in the template text.
+     *
+     * @memberOf _.templateSettings
+     * @type string
+     */
+    'variable': '',
+
+    /**
+     * Used to import variables into the compiled template.
+     *
+     * @memberOf _.templateSettings
+     * @type Object
+     */
+    'imports': {
+
+      /**
+       * A reference to the `lodash` function.
+       *
+       * @memberOf _.templateSettings.imports
+       * @type Function
+       */
+      '_': { 'escape': escape }
+    }
+  };
+
+  return templateSettings;
+});
