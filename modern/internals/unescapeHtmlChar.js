e2f29e12f38a3dfbd55a4007469653be704d01ea
John-David Dalton <john.david.dalton@gmail.com>
2014-04-13 15:18:53 -0700

    Add modules that are inlined for devs to use separately.

---
+++ modern/internals/unescapeHtmlChar.js
@@ -0,0 +1,32 @@
+/**
+ * Lo-Dash 2.4.1 (Custom Build) <http://lodash.com/>
+ * Build: `lodash modularize modern exports="amd" -o ./modern/`
+ * Copyright 2012-2014 The Dojo Foundation <http://dojofoundation.org/>
+ * Based on Underscore.js 1.6.0 <http://underscorejs.org/LICENSE>
+ * Copyright 2009-2014 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+ * Available under MIT license <http://lodash.com/license>
+ */
+define([], function() {
+
+  /** Used to convert HTML entities to characters */
+  var htmlUnescapes = {
+    '&amp;': '&',
+    '&lt;': '<',
+    '&gt;': '>',
+    '&quot;': '"',
+    '&#39;': "'"
+  };
+
+  /**
+   * Used by `_.unescape` to convert HTML entities to characters.
+   *
+   * @private
+   * @param {string} chr The matched character to unescape.
+   * @returns {string} Returns the unescaped character.
+   */
+  function unescapeHtmlChar(chr) {
+    return htmlUnescapes[chr];
+  }
+
+  return unescapeHtmlChar;
+});
