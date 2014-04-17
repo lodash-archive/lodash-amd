2685cb4c44103693621cec248207cce3bb453c37
John-David Dalton <john.david.dalton@gmail.com>
2014-04-17 07:34:53 -0700

    Ensure `difference` works with arrays regardless of arg position.

--- compat/arrays/difference.js
+++ compat/arrays/difference.js
@@ -6,7 +6,7 @@
  * Copyright 2009-2014 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
  * Available under MIT license <http://lodash.com/license>
  */
-define(['../internals/baseDifference', '../internals/baseFlatten'], function(baseDifference, baseFlatten) {
+define(['../internals/baseDifference', '../internals/baseFlatten', '../objects/isArguments', '../objects/isArray'], function(baseDifference, baseFlatten, isArguments, isArray) {
 
   /**
    * Creates an array excluding all values of the provided arrays using strict
@@ -24,7 +24,16 @@ define(['../internals/baseDifference', '../internals/baseFlatten'], function(bas
    * // => [1, 3]
    */
   function difference() {
-    return baseDifference(arguments[0], baseFlatten(arguments, true, true, 1));
+    var index = -1,
+        length = arguments.length;
+
+    while (++index < length) {
+      var value = arguments[index];
+      if (isArray(value) || isArguments(value)) {
+        break;
+      }
+    }
+    return baseDifference(arguments[index], baseFlatten(arguments, true, true, ++index));
   }
 
   return difference;
