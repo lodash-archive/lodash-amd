c421343997a3381bd34ea959fde5ce46dba297f9
John-David Dalton <john.david.dalton@gmail.com>
2014-04-29 08:29:20 -0700

    Make `merge` align with `assign` exit early snippet.

--- modern/objects/merge.js
+++ modern/objects/merge.js
@@ -125,13 +125,13 @@ define(['../internals/arrayEach', '../internals/baseCreateCallback', '../interna
    * // => { 'fruits': ['apple', 'banana'], 'vegetables': ['beet', 'carrot] }
    */
   function merge(object, source, guard) {
-    if (!object) {
-      return object;
-    }
     var args = arguments,
         length = args.length,
         type = typeof guard;
 
+    if (!object || length < 2) {
+      return object;
+    }
     // enables use as a callback for functions like `_.reduce`
     if ((type == 'number' || type == 'string') && args[3] && args[3][guard] === source) {
       length = 2;
