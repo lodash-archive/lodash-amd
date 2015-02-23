6151773362dde3e30f0282581105198ed00d5b53
jdalton <john.david.dalton@gmail.com>
2015-02-23 08:34:28 -0800

    Bump to v3.3.1.

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-/**
- * Lo-Dash 3.3.0 (Custom Build) <https://lodash.com/>
- * Copyright 2012-2015 The Dojo Foundation <http://dojofoundation.org/>
- * Based on Underscore.js 1.7.0 <http://underscorejs.org/LICENSE>
- * Copyright 2009-2015 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
- * Available under MIT license <https://lodash.com/license>
- */
 define(['./modern/main'], function(lodash) {
   return lodash;
 });
